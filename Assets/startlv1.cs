766bf9acb8d77b54717a34002b8c9db444dc200b
Ireneusz Pronkiewicz <50549535+iHaczked@users.noreply.github.com>
2024-01-12 21:38:53 +0100

    dodanie menu i innych rzeczy

---
+++ Assets/startlv1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class startlv1 : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("level1");
+    }
+}
---
+++ Assets/wyjdz.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
