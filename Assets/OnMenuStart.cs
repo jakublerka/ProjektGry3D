4b9febc51e16e8e49c6af2ee934a8fae51311790
tecebe <artmaz64@gmail.com>
2024-01-13 11:40:23 +0100

    quick fix :)

---
+++ Assets/OnMenuStart.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Example : MonoBehaviour
+{
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
